d145916343cf57393417799b4bfc7609595d35be
Saurabh <f20212412@pilani.bits-pilani.ac.in>
2022-05-08 15:40:14 +0530

    made minor changes

--- SARSA.py
+++ SARSA.py
@@ -15,9 +15,9 @@ class SARSA():
         self.n_states=env.observation_space.n
         self.Q=np.zeros((self.n_states,self.n_actions))
         
-    def epsilon_greedy_policy(self,state):
+    def epsilon_greedy_policy(self,state,epsilon):
         
-        if random.uniform(0,1)<self.epsilon:
+        if random.uniform(0,1)<epsilon:
             return self.env.action_space.sample()
         
         else:
@@ -26,17 +26,28 @@ class SARSA():
     def train(self):
         for i in range(self.n_episodes):
             s=self.env.reset()
-            a=self.epsilon_greedy_policy(s)
+            a=self.epsilon_greedy_policy(s,self.epsilon)
             for t in range(self.n_timesteps):
                 s_,r,done,_=self.env.step(a)
-                a_=self.epsilon_greedy_policy(s_)
+                a_=self.epsilon_greedy_policy(s_,self.epsilon)
                 self.Q[s][a] += self.alpha*(r+self.gamma*self.Q[s_][a_]-self.Q[s][a])
                 s=s_
                 a=a_
                 if done:
                     break
-model=SARSA(env,epsilon=0.8,alpha=0.85,gamma=0.9,episodes=50000,timesteps=1000)
+    
+    def test(self,n_tests):
+        for i in range(n_tests):
+            s=self.env.reset()
+            a=self.epsilon_greedy_policy(s,0)
+            for t in range(self.n_timesteps):
+                s,r,done,_=self.env.step(a)
+                a=self.epsilon_greedy_policy(s,0)
+                if done:
+                    self.env.render()
+                    break
+                
+model=SARSA(env,epsilon=0.5,alpha=0.85,gamma=0.9,episodes=500000,timesteps=1000)
 model.train()
-print(model.Q)        
-
+model.test(10)
 
